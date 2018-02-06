7ba81afdccf0d4f1635e8575e0605c9827b81f1c
itggot-emil-sonksen <emil.sonksen@itggot.se>
2018-02-06 10:51:13 +0100

    Added subpages and their stylesheets. Also linked them together.

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,7 +1,4 @@
 function toggleMenu(){
     var element = document.querySelector(".dropdown");
     element.classList.toggle("show");
-
-    element = document.querySelector("main");
-    element.classList.toggle("fade")
 }

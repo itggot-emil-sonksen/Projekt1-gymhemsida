9e14e9474d6969f9fc523ba9f3b0ca7df70f0c20
itggot-emil-sonksen <emil.sonksen@itggot.se>
2018-03-16 13:44:47 +0100

    Tried photoshop, didn't go well

---
+++ site/public/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".dropdown");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".fade-wrap");
+    element.classList.toggle("visible");
+}
